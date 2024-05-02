085c9b1e6260b5e174417ac2e03041a361d22be7
jjc1 <jjc1@williams.edu>
2024-05-02 13:41:11 -0400

    Imported SymbolTable interface and created abstract methods in ScapegoatTree.

--- Ao3Analyzer/ScapegoatTree.java
+++ Ao3Analyzer/ScapegoatTree.java
@@ -3,14 +3,14 @@ package Ao3Analyzer;
 import java.io.*;
 import java.util.*;
 
-public class BinarySearchTree<Key, Value> implements SymbolTable<Key, Value>{
+public class ScapegoatTree<Key, Value> implements SymbolTable<Key, Value>{
 	
 	private Node root;
 	private int size;
     private int maxSize;
     private Node scapegoat;
 
-	public BinarySearchTree(){
+	public ScapegoatTree(){
 		this.size = 0;
 	}
 
@@ -31,6 +31,38 @@ public class BinarySearchTree<Key, Value> implements SymbolTable<Key, Value>{
             this.size = size;
             this.isScapegoat = isScapegoat;
         } 
+    }
+
+    public boolean isEmpty(){
+        // To do
+        return false;
+    }
+
+    // Returns the number of keys 
+    public int size(){
+        // To do
+        return 0;
+    }
+
+    // Puts the key-value pair into the table 
+    // The comparator is used to compare keys
+    public void put(Key key, Value val, Comparator<Key> comparator){
+        // To do
+        System.out.println("Not implemented yet");
+    } 
+
+    // Returns the value paired with the key 
+    // Returns null if the key is not in the table
+    // The comparator is used to compare keys
+    // "Search operation" 
+    public Value get(Key key, Comparator<Key> comparator){
+        // To do
+        return null;
+    }
+
+    public void delete(Key key, Comparator<Key> comparator){
+        // To do
+        System.out.println("Not implemented yet");
     }
 
     public static void main(String[] args) {
---
+++ Ao3Analyzer/SymbolTable.java
@@ -0,0 +1,24 @@
+package Ao3Analyzer; 
+
+import java.util.Comparator;
+
+public interface SymbolTable<Key, Value>{
+
+	// Returns true if the table is empty  
+	public abstract boolean isEmpty();
+
+	// Returns the number of keys 
+	public abstract int size();
+
+	// Puts the key-value pair into the table 
+	// The comparator is used to compare keys
+	public abstract void put(Key key, Value val, Comparator<Key> comparator); 
+
+	// Returns the value paired with the key 
+	// Returns null if the key is not in the table
+	// The comparator is used to compare keys
+	// "Search operation" 
+	public abstract Value get(Key key, Comparator<Key> comparator);
+
+	public abstract void delete(Key key, Comparator<Key> comparator);
+}
